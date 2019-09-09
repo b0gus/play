bea92e2dcee0c54a641326fa71062b321b1ff0fb
b0gus <suh0317@korea.ac.kr>
2019-09-09 18:10:55 +0900

    !

---
+++ toy/nba/elo.py
@@ -0,0 +1,35 @@
+import json
+import operator
+from pprint import pprint as pp
+
+f = open("2018-19.txt")
+data = f.readlines()
+tmp = {}
+
+for i in data:
+    if i[0] == "2":
+        tmp.setdefault(i.split()[3], 1500)
+
+def cal_elo(w, l):
+    K = 16
+    Ew = 1 / (1 + 10**((l-w)/400))
+    El = 1 / (1 + 10**((w-l)/400))
+    print(Ew, El)
+    w = w + K*(1-Ew)
+    l = l + K*(0-El)
+    return w, l
+
+for i in data:
+    if len(i) == 29:
+        w = i.split()[3]
+        l = i.split()[6]
+        w_elo, l_elo = cal_elo(tmp[w], tmp[l])
+        tmp[w] = w_elo
+        tmp[l] = l_elo
+
+nba = sorted(tmp.items(), key=operator.itemgetter(1), reverse=True)
+
+pp(nba)
+
+# with open('nba.json', 'wb') as f:
+#     json.dump(tmp, f)
