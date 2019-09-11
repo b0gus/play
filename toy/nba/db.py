8990570559bce600ee0efb4478fe5bb0a55e367f
bogus <uhs0317@naver.com>
2019-09-11 18:54:11 +0900

    ^^

--- toy/nba/db.py
+++ toy/nba/db.py
@@ -8,26 +8,24 @@ c = conn.cursor()
 # c.execute('''CREATE TABLE game
 #              (date text, winner text, loser text)''')
 
-f = open("./info/1946-47.txt")
-tmp = f.readlines()
-f.close()
-
-for i in tmp:
-    if i.split()[0][0] != "=":
-        date = i.split()[0]
-        w = i.split()[3]
-        l = i.split()[6]
-        c.execute("INSERT INTO game VALUES ('{}', '{}', '{}')".format(date, w, l))
+for i in range(0, 9): # 0809
+    year = i
+    season = "201{}-1{}".format(year, year+1)
+    f = open("./info_edited/{}.txt".format(season))
+    tmp = f.readlines()
+    f.close()
+    for i in tmp:
+        if i.split()[0][0] != "=":
+            date = i.split()[0]
+            w = i.split()[3]
+            l = i.split()[6]
+            c.execute("INSERT INTO game VALUES ('{}', '{}', '{}')".format(date, w, l))
 
 # # Insert a row of data
 # c.execute("INSERT INTO nba VALUES ({}, {}, {})".format(date, w, l))
 
 # Save (commit) the changes
 conn.commit()
-
-c.execute("SELECT * FROM game")
-for i in c:
-    print(i)
 
 # We can also close the connection if we are done with it.
 # Just be sure any changes have been committed or they will be lost.
